257c2e9ad34faaea31b7f6bd404ff6ff06182058
Andrey Tvorozhkov <andrey@h-labs.ru>
2022-01-09 22:47:26 +0300

    Fixes

--- pre-build.sh
+++ pre-build.sh
@@ -9,3 +9,7 @@ docker build -f Dockerfile -t ton-base .
 docker tag ton-base ${REGISTRY}/ton/ton-base:${VERSION}
 docker push ${REGISTRY}/ton/ton-base:${VERSION}
 
+cd ../ton-full-node
+docker build -f Dockerfile -t ton-node .
+docker tag ton-base ${REGISTRY}/ton/ton-node:${VERSION}
+docker push ${REGISTRY}/ton/ton-node:${VERSION}
