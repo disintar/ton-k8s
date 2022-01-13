3b1e5bec05b8cf3e0483f0990b28b310940a3bc3
Andrey Tvorozhkov <andrey@h-labs.ru>
2022-01-13 18:38:55 +0300

    Base applications check UDP is running

--- ton-compile-source/scripts/init_ton.py
+++ ton-compile-source/scripts/init_ton.py
@@ -1,4 +1,7 @@
-import time
+import select
+import socket
+
+ip = "0.0.0.0"
 
 
 class KeyStorage:
@@ -11,6 +14,23 @@ class KeyStorage:
         pass
 
 
+sockets = {}
+
+for port in [46731, 50000, 43679, 46732, 50001, 43680]:
+    # Create a UDP socket
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+    server_address = (ip, port)
+    s.bind(server_address)
+    sockets[port] = s
+
+print("👋 Hi there!")
+
 while True:
-    print("👋 Hi there!")
-    time.sleep(60)
+    for port, s in sockets.items():
+        ready = select.select([s], [], [], 0.1)
+        if ready[0]:
+            data, address = s.recvfrom(4096)
+
+            print(f"Server {port} received: ", data.decode('utf-8'))
+            s.sendto("👋".encode('utf-8'), address)
