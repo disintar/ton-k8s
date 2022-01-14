e76a4e6723bb9bbc285fef025ff032972a67f348
Andrey Tvorozhkov <andrey@h-labs.ru>
2022-01-14 15:01:56 +0300

    Fix

--- ton-compile-source/scripts/init_ton.py
+++ ton-compile-source/scripts/init_ton.py
@@ -1,8 +1,6 @@
-import select
-import socket
 from hllib.log import logger
 from hllib.net import get_my_ip
-
+from hllib.udp_port_example import listen_ports
 
 ip = get_my_ip()
 
@@ -21,21 +19,5 @@ class KeyStorage:
         pass
 
 
-sockets = {}
-
-for port in [46731, 50000, 43679, 46732, 50001, 43680]:
-    # Create a UDP socket
-    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-
-    server_address = ("0.0.0.0", port)
-    s.bind(server_address)
-    sockets[port] = s
-
-while True:
-    for port, s in sockets.items():
-        ready = select.select([s], [], [], 0.1)
-        if ready[0]:
-            data, address = s.recvfrom(4096)
-
-            logger.info(f"Server {port} received: ", data.decode('utf-8'))
-            s.sendto("👋".encode('utf-8'), address)
+if __name__ == "__main__":
+    listen_ports()
