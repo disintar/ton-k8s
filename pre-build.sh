498dd824b6a712a4b71367c666a4947f883db107
Andrey Tvorozhkov <andrey@h-labs.ru>
2022-01-14 12:15:41 +0300

    Add PUSH_TO_REGISTRY setting in pre build

--- pre-build.sh
+++ pre-build.sh
@@ -1,24 +1,37 @@
 export REGISTRY=registry.head-labs.com
+export PUSH_TO_REGISTRY=false
 export VERSION=v0
 
 # Build base compile source
 
 cd ton-compile-source
 
-docker build -f Dockerfile -t ton-base . --build-arg is_testnet=false
-docker tag ton-base ${REGISTRY}/ton/ton-base:mainnet-${VERSION}
-docker push ${REGISTRY}/ton/ton-base:mainnet-${VERSION}
+docker build -f Dockerfile -t ton-base-mainnet . --build-arg is_testnet=false
 
-docker build -f Dockerfile -t ton-base . --build-arg is_testnet=true
-docker tag ton-base ${REGISTRY}/ton/ton-base:testnet-${VERSION}
-docker push ${REGISTRY}/ton/ton-base:testnet-${VERSION}
+if PUSH_TO_REGISTRY; then
+  docker tag ton-base-mainnet ${REGISTRY}/ton/ton-base:mainnet-${VERSION}
+  docker push ${REGISTRY}/ton/ton-base:mainnet-${VERSION}
+fi;
+
+docker build -f Dockerfile -t ton-base-testnet . --build-arg is_testnet=true
+
+if PUSH_TO_REGISTRY; then
+  docker tag ton-base-testnet ${REGISTRY}/ton/ton-base:testnet-${VERSION}
+  docker push ${REGISTRY}/ton/ton-base:testnet-${VERSION}
+fi;
 
 cd ../ton-full-node
 
-docker build -f Dockerfile -t ton-node . --build-arg version=mainnet-${VERSION}
-docker tag ton-base ${REGISTRY}/ton/ton-node:mainnet-${VERSION}
-docker push ${REGISTRY}/ton/ton-node:mainnet-${VERSION}
+docker build -f Dockerfile -t ton-node-mainnet . --build-arg version=mainnet-${VERSION}
 
-docker build -f Dockerfile -t ton-node . --build-arg version=testnet-${VERSION}
-docker tag ton-base ${REGISTRY}/ton/ton-node:testnet-${VERSION}
-docker push ${REGISTRY}/ton/ton-node:testnet-${VERSION}
+if PUSH_TO_REGISTRY; then
+  docker tag ton-node-mainnet ${REGISTRY}/ton/ton-node:mainnet-${VERSION}
+  docker push ${REGISTRY}/ton/ton-node:mainnet-${VERSION}
+fi;
+
+docker build -f Dockerfile -t ton-node-testnet . --build-arg version=testnet-${VERSION}
+
+if PUSH_TO_REGISTRY; then
+  docker tag ton-node-testnet ${REGISTRY}/ton/ton-node:testnet-${VERSION}
+  docker push ${REGISTRY}/ton/ton-node:testnet-${VERSION}
+fi;
--- ton-compile-source/scripts/lib/net.py
+++ ton-compile-source/scripts/lib/net.py
@@ -4,6 +4,8 @@ import requests
 
 
 def get_my_ip() -> Optional[str]:
+    """Return public IP address"""
+
     response = requests.get("https://ifconfig.me")
 
     if response.status_code == 200:
