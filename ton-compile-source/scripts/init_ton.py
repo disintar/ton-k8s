6f6e19deaf4bd479c4a62eac3d806af54bd0cc56
Andrey Tvorozhkov <andrey@h-labs.ru>
2022-01-15 17:09:22 +0300

    Some fixes in helm chart

--- ton-compile-source/scripts/init_ton.py
+++ ton-compile-source/scripts/init_ton.py
@@ -18,7 +18,7 @@ config = {
     "PUBLIC_PORT": int(os.getenv("PUBLIC_PORT", 50001)),
     "LITESERVER_PORT": int(os.getenv("LITESERVER_PORT", 43680)),
     "THREADS": cpu_count,
-    "VERBOSE": 3
+    "VERBOSE": 5
 }
 
 logger.info("👋 Hi there!\n"
