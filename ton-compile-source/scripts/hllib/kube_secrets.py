3f2f78750e2165fa3d6ec0190d23b381e6ac0430
Andrey Tvorozhkov <andrey@h-labs.ru>
2022-01-17 14:52:29 +0300

    Add service account / secret mount

--- ton-compile-source/scripts/hllib/kube_secrets.py
+++ ton-compile-source/scripts/hllib/kube_secrets.py
@@ -2,9 +2,29 @@ from kubernetes import client, config
 
 
 class KubeConnector:
-    def __init__(self):
-        config.load_kube_config()
-        self.v1 = client.CoreV1Api()
+    def __init__(self, namespace: str):
+        """
+        Interact with kubernetes secrets
+        :param namespace:
+        """
 
-    def get_secret(name: str):
-        pass
+        configuration = client.Configuration()
+
+        token = open('/etc/secret-volume/token').read()
+        host = 'https://kubernetes.default.svc'
+        ssl_ca_cert = '/etc/secret-volume/ca.crt'
+
+        configuration.api_key["authorization"] = token
+        configuration.api_key_prefix['authorization'] = 'Bearer'
+        configuration.host = host
+        configuration.ssl_ca_cert = ssl_ca_cert
+
+        self.api = client.CoreV1Api(client.ApiClient(configuration))
+        self.v1 = client.CoreV1Api()
+        self.namespace = namespace
+
+    def get_secret(self, name: str):
+        return self.v1.read_namespaced_secret(name, self.namespace)
+
+    def update_secret(self, name: str, key: str, value: str):
+        return self.v1.replace_namespaced_secret(name, self.namespace, None)
