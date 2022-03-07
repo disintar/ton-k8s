b3cbb0eba4baa2d9464b71da76e7a37150bfc319
Andrey Tvorozhkov <andrey@h-labs.ru>
2022-03-07 12:15:09 +0300

    Fix

--- ton-compile-source/scripts/hllib/command_line.py
+++ ton-compile-source/scripts/hllib/command_line.py
@@ -8,14 +8,23 @@ def run(command: List[str], cwd: str = None) -> Optional[str]:
     """Run command and return output"""
 
     try:
-        command = list(map(str, command))
-        logger.debug(f"🐼 Run: {' '.join(command)}")
-        get_output = subprocess.check_output(command, cwd=cwd)
+        output_good = False
 
-        if get_output:
+        while not output_good:
+            command = list(map(str, command))
+            logger.debug(f"🐼 Run: {' '.join(command)}")
+            get_output = subprocess.check_output(command, cwd=cwd)
             answer = get_output.decode()
-            logger.debug(f"🐼 Answer: {answer}")
-            return answer
+
+            if 'PosixError' in answer:
+                logger.error(f"🐗 Answer is not good: {answer}")
+                continue
+            else:
+                get_output = True
+
+            if get_output:
+                logger.debug(f"🐼 Answer: {answer}")
+                return answer
     except subprocess.CalledProcessError as exc:
         logger.error(f"Status : FAIL {exc.returncode} {exc.output}")
         raise exc
