2425aff6d485a4e50bca8763b7e1779f7df869cb
Andrey Tvorozhkov <andrey@h-labs.ru>
2022-03-07 18:01:36 +0300

    Fix sleep

--- ton-compile-source/scripts/hllib/net.py
+++ ton-compile-source/scripts/hllib/net.py
@@ -6,8 +6,8 @@ from requests.adapters import HTTPAdapter, Retry
 
 r = requests.Session()
 
-retries = Retry(total=10,
-                backoff_factor=0.1,
+retries = Retry(total=5,
+                backoff_factor=1,
                 status_forcelist=[ 500, 502, 503, 504 ])
 
 r.mount('http://', HTTPAdapter(max_retries=retries))
