290c04e8981549a56de720734fa40fdb36a02fae
Andrey Tvorozhkov <andrey@h-labs.ru>
2022-07-25 11:46:07 +0300

    Remove testnet from git

--- build-for-k8s.sh
+++ build-for-k8s.sh
@@ -12,12 +12,12 @@ docker build -f Dockerfile -t ton-base-mainnet . --build-arg is_testnet=false --
 
 docker tag ton-base-mainnet ${REGISTRY}/ton/ton-base:mainnet-${VERSION}
 docker push ${REGISTRY}/ton/ton-base:mainnet-${VERSION}
-
-# testnet
-docker build -f Dockerfile -t ton-base-testnet . --build-arg is_testnet=true --build-arg repo=ssh://git@git.head-labs.com:228/disintar/ton.git
-
-docker tag ton-base-testnet ${REGISTRY}/ton/ton-base:testnet-${VERSION}
-docker push ${REGISTRY}/ton/ton-base:testnet-${VERSION}
+#
+## testnet
+#docker build -f Dockerfile -t ton-base-testnet . --build-arg is_testnet=true --build-arg repo=ssh://git@git.head-labs.com:228/disintar/ton.git
+#
+#docker tag ton-base-testnet ${REGISTRY}/ton/ton-base:testnet-${VERSION}
+#docker push ${REGISTRY}/ton/ton-base:testnet-${VERSION}
 
 cd ../ton-full-node || return
 
@@ -25,11 +25,11 @@ cd ../ton-full-node || return
 docker build -f Dockerfile -t ton-node-mainnet . --build-arg base=${REGISTRY}/ton/ton-base:mainnet-${VERSION}
 docker tag ton-node-mainnet ${REGISTRY}/ton/ton-node:mainnet-${VERSION}
 docker push ${REGISTRY}/ton/ton-node:mainnet-${VERSION}
-
-# fullnode testnet
-docker build -f Dockerfile -t ton-node-testnet . --build-arg base=${REGISTRY}/ton/ton-base:testnet-${VERSION}
-docker tag ton-node-testnet ${REGISTRY}/ton/ton-node:testnet-${VERSION}
-docker push ${REGISTRY}/ton/ton-node:testnet-${VERSION}
+#
+## fullnode testnet
+#docker build -f Dockerfile -t ton-node-testnet . --build-arg base=${REGISTRY}/ton/ton-base:testnet-${VERSION}
+#docker tag ton-node-testnet ${REGISTRY}/ton/ton-node:testnet-${VERSION}
+#docker push ${REGISTRY}/ton/ton-node:testnet-${VERSION}
 
 cd ../ton-http-config || return
 
@@ -45,8 +45,8 @@ cd ../ton-toncenter || return
 docker build -f Dockerfile -t ton-toncenter-mainnet . --build-arg base=${REGISTRY}/ton/ton-base:mainnet-${VERSION}
 docker tag ton-toncenter-mainnet ${REGISTRY}/ton/ton-toncenter:mainnet-${VERSION}
 docker push ${REGISTRY}/ton/ton-toncenter:mainnet-${VERSION}
-
-# toncenter testnet
-docker build -f Dockerfile -t ton-toncenter-testnet . --build-arg base=${REGISTRY}/ton/ton-base:testnet-${VERSION}
-docker tag ton-toncenter-testnet ${REGISTRY}/ton/ton-toncenter:testnet-${VERSION}
-docker push ${REGISTRY}/ton/ton-toncenter:testnet-${VERSION}
+#
+## toncenter testnet
+#docker build -f Dockerfile -t ton-toncenter-testnet . --build-arg base=${REGISTRY}/ton/ton-base:testnet-${VERSION}
+#docker tag ton-toncenter-testnet ${REGISTRY}/ton/ton-toncenter:testnet-${VERSION}
+#docker push ${REGISTRY}/ton/ton-toncenter:testnet-${VERSION}
